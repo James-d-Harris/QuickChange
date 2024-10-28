b6564d1c497efe9862ebcc8478c21312d971871e
Tate <trw286@nau.edu>
2024-10-27 23:19:15 -0700

    Updated coinClass.cs
    Added update function to track coin move movement.
    Added snap function to snap to middle of collider.

--- Scripts/Game/coinClass.cs
+++ Scripts/Game/coinClass.cs
@@ -5,56 +5,146 @@ using System.Collections;
 public class Currency : MonoBehaviour
 {
 	// define attributes
-	public string name;
+	public string currencyName;
 	public double monetaryValue;
+	Vector3 mousePosition;
+	public float moveSpeed = 0.1f;
+	RaycastHit2D rb;
+	public bool isDragging = false;
+    public Transform tableArea;
+    public float snapThreshold = .05f;
 
-	// currency constructor
-	public Currency( string name, double value )
+	// update method
+	public void Update()
 	{
-		// initialize variables
-		this.name = name;
-		this.monetaryValue = value;
+		// check if left mouse button is clicked and over object
+		if ( Input.GetMouseButtonDown( 0 ) )
+		{
+			// get mouse position in world space
+			mousePosition = Input.mousePosition;
+
+			// move mouse to that position
+			mousePosition = Camera.main.ScreenToWorldPoint( mousePosition );
+
+			// check if mouse if over object using raycast
+			rb = Physics2D.Raycast( mousePosition, Vector2.zero );
+
+			// ensure mouse has hit a collider 
+			if ( rb.collider != null && rb.collider.gameObject == gameObject )
+			{
+				// set isDragging to true
+				isDragging = true;
+			}
+		}
+
+		// check if left mouse button has been released
+		if ( Input.GetMouseButtonUp( 0 ) )
+		{
+			// set isDragging to false
+			isDragging = false;
+
+            // snap to collider
+            SnapToTable();
+		}
+
+		// if dragging is true, move object with mouse
+		if ( isDragging )
+		{
+			// update mouse position
+			mousePosition = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+
+			// ensure z component is negative (2D game)
+			mousePosition.z = 0;
+
+			// set object position to mouse position to ensure that it follows the cursor
+			transform.position = Vector3.Lerp( transform.position, mousePosition, moveSpeed );
+		}
 	}
+
+    public void SnapToTable()
+    {
+        // check distance to the table area
+        if ( Vector3.Distance( transform.position, tableArea.position ) < snapThreshold )
+        {
+            // snap to the table's position
+            transform.position = tableArea.position;
+        }
+    }
 
 	// penny class
 	public class Penny : Currency
 	{
-		public Penny() : base( "Penny", 0.01 ) { }
+        // initialize using Start function
+		void Start()
+        {
+            currencyName = "Penny";
+            monetaryValue = 0.01;
+        }
 	}
 
 	// nickel class
 	public class Nickel : Currency
 	{
-		public Nickel() : base( "Nickel", 0.05 ) { }
+		 // initialize using Start function
+		void Start()
+        {
+            currencyName = "Nickel";
+            monetaryValue = 0.05;
+        }
 	}
 
 	// dime class
 	public class Dime : Currency
 	{
-		public Dime() : base( "Dime", 0.1 ) { }
+		 // initialize using Start function
+		void Start()
+        {
+            currencyName = "Dime";
+            monetaryValue = 0.1;
+        }
 	}
 
 	// quarter class
 	public class Quarter : Currency
 	{
-		public Quarter() : base( "Quarter", 0.25 ) { }
+		 // initialize using Start function
+		void Start()
+        {
+            currencyName = "Quarter";
+            monetaryValue = 0.25;
+        }
 	}
 
 	// one dollar class
 	public class OneDollar : Currency
 	{
-		public OneDollar() : base( "One Dollar", 1.00 ) { }
+		 // initialize using Start function
+		void Start()
+        {
+            currencyName = "One Dollar";
+            monetaryValue = 1.00;
+        }
 	}
 
 	// five dollar class
 	public class FiveDollar : Currency
 	{
-		public FiveDollar() : base( "Five Dollar", 5.00 ) { }
+		 // initialize using Start function
+		void Start()
+        {
+            currencyName = "Five Dollar";
+            monetaryValue = 5.00;
+        }
 	}
 
 	// ten dollar class
 	public class TenDollar : Currency
 	{
-		public TenDollar() : base( "Ten Dollar", 10.00 ) { }
+		 // initialize using Start function
+		void Start()
+        {
+            currencyName = "Ten Dollar";
+            monetaryValue = 10.00;
+        }
 	}
 }
